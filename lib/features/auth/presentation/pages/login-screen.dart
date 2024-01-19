6ceda01e1df7cd6d39fe90fa84aa11e7d7383201
smkhaled <khaledsaidmk@gmail.com>
2024-01-19 09:45:51 +0400

    adjust login ui

--- lib/features/auth/presentation/pages/login-screen.dart
+++ lib/features/auth/presentation/pages/login-screen.dart
@@ -16,152 +16,161 @@ class LoginScreen extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     return  Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 400,
+        body: Stack(
+          children: [
+            Positioned(
+              right: 0,
+              left: 0,
+              child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(143, 148, 251, .6),
+                      Color.fromRGBO(143, 148, 251, 1),
+
+                    ]
+                ) ),
+                height: 300,
+              ),
+            ),
+            Positioned(
+                left: 30,
+                width: 80,
+                height: 200,
+                child: Container(
                   decoration: const BoxDecoration(
                       image: DecorationImage(
-                          image: AssetImage('assets/images/background.png'),
-                          fit: BoxFit.fill
+                          image: AssetImage('assets/images/light-1.png')
                       )
-                  ),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned(
-                          left: 30,
-                          width: 80,
-                          height: 200,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage('assets/images/light-1.png')
-                                )
-                            ),
-                          )
-                      ),
-                      Positioned(
-                          left: 140,
-                          width: 80,
-                          height: 150,
-                          child:  Container(
-                            decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage('assets/images/light-2.png')
-                                )
-                            ),
-                          )
-                      ),
-                      Positioned(
-                          right: 40,
-                          top: 40,
-                          width: 80,
-                          height: 150,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage('assets/images/clock.png')
-                                )
-                            ),
-                          )
-                      ),
-                      Positioned(
-                          child:  Container(
-                            margin: const EdgeInsets.only(top: 50),
-                            child: const Center(
-                              child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
-                            ),
-                          )
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              const BoxShadow(
-                                  color: Color.fromRGBO(143, 148, 251, .2),
-                                  blurRadius: 20.0,
-                                  offset: Offset(0, 10)
-                              )
-                            ]
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              padding: const EdgeInsets.all(8.0),
-                              decoration: const BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey))
-                              ),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Email or Phone number",
-                                    hintStyle: TextStyle(color: Colors.grey[400])
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(8.0),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(color: Colors.grey[400])
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      const Gap(30),
-                      Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            gradient: const LinearGradient(
-                                colors: [
-                                  Color.fromRGBO(143, 148, 251, 1),
-                                  Color.fromRGBO(143, 148, 251, .6),
-                                ]
-                            )
-                        ),
-                        child: const Center(
-                          child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                        ),
-                      ),
-                      const Gap(15),
-                      const Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),),
-
-                      const Gap(30),
-                      SizedBox(
-                        width: width,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text("Don\'t have an account?",style: TextStyle(color: Colors.grey,fontSize: 14),),
-                            // const Gap(10),
-                            TextButton(
-                                onPressed: (){},
-                                child: const Text("REGISTER NOW",style: TextStyle(color: Colors.deepPurpleAccent,fontWeight: FontWeight.bold),))
-                          ],),
-                      ),
-
-                    ],
                   ),
                 )
-              ],
             ),
-          ),
+            Positioned(
+                left: 140,
+                width: 80,
+                height: 150,
+                child:  Container(
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/light-2.png')
+                      )
+                  ),
+                )
+            ),
+            Positioned(
+                right: 40,
+                top: 40,
+                width: 80,
+                height: 150,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/clock.png')
+                      )
+                  ),
+                )
+            ),
+            Positioned(
+              top: 250,
+              right: 0,
+              left: 0,
+              bottom: 0,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60))
+                ),
+                child: Container(
+                  height: 400,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const Gap(30),
+                          Container(
+                            padding: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  const BoxShadow(
+                                      color: Color.fromRGBO(143, 148, 251, .2),
+                                      blurRadius: 20.0,
+                                      offset: Offset(0, 10)
+                                  )
+                                ]
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  decoration: const BoxDecoration(
+                                      border: Border(bottom: BorderSide(color: Colors.grey))
+                                  ),
+                                  child: TextFormField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Email or Phone number",
+                                        hintStyle: TextStyle(color: Colors.grey[400])
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Password",
+                                        hintStyle: TextStyle(color: Colors.grey[400])
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const Gap(30),
+                          Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: const LinearGradient(
+                                    colors: [
+                                      Color.fromRGBO(143, 148, 251, 1),
+                                      Color.fromRGBO(143, 148, 251, .6),
+                                    ]
+                                )
+                            ),
+                            child: const Center(
+                              child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                          ),
+                          const Gap(15),
+                          const Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),),
+                      
+                          const Gap(30),
+                          SizedBox(
+                            width: width,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text("Don\'t have an account?",style: TextStyle(color: Colors.grey,fontSize: 14),),
+                                // const Gap(10),
+                                TextButton(
+                                    onPressed: (){},
+                                    child: const Text("REGISTER NOW",style: TextStyle(color: Colors.deepPurpleAccent,fontWeight: FontWeight.bold),))
+                              ],),
+                          ),
+                      
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ),
+            ),
+
+          ],
         )
     );
   }
